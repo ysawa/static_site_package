98fb702c5770e6abd2b8272a322c7f369b6648af
Yoshihiro Sawa <yoshihirosawa@gmail.com>
2013-02-03 18:13:49 +0900

    add: set include paths

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
+<?php set_include_path(dirname(__FILE__) . DIRECTORY_SEPARATOR . '/templates' . PATH_SEPARATOR . get_include_path()); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 <head>
-<title>Hello world!</title>
+<?php include 'head.php'; ?>
 </head>
 <body>
 <h1>
