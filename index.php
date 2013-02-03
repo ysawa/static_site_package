b62456972153da531545490af5392b998b63b11b
Yoshihiro Sawa <yoshihirosawa@gmail.com>
2013-02-03 18:25:46 +0900

    modify: head tags

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php set_include_path(dirname(__FILE__) . DIRECTORY_SEPARATOR . '/templates' . PATH_SEPARATOR . get_include_path()); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html>
+<html lang='ja'>
 <head>
 <?php include 'head.php'; ?>
 </head>
 <body>
 <h1>
-Hello world!
+Hello, world!
 </h1>
 </body>
 </html>
--- templates/head.php
+++ templates/head.php
@@ -1,1 +1,7 @@
-<title>Hello world!</title>
+<meta charset='UTF-8' />
+<title>Hello, world!</title>
+<meta content='Hello World Page' name='description' />
+<meta content='Hello,World' name='keywords' />
+<link href='./assets/css/app.css' rel='stylesheet' type='text/css' />
+<script src='./assets/js/jquery-1.9.0.min.js' type='text/javascript'></script>
+<script src='./assets/js/app.js' type='text/javascript'></script>
