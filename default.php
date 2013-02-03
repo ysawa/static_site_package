5c12ef9bd16633a5180e40cba4021d58a6f6b520
Yoshihiro Sawa <yoshihirosawa@gmail.com>
2013-02-03 19:05:53 +0900

    modify: templates

---
+++ default.php
@@ -0,0 +1,2 @@
+<?php define('ROOT_PATH', '/static_site_package/'); ?>
+<?php set_include_path(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'templates' . PATH_SEPARATOR . get_include_path()); ?>
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php set_include_path(dirname(__FILE__) . DIRECTORY_SEPARATOR . '/templates' . PATH_SEPARATOR . get_include_path()); ?>
+<?php require_once './default.php'; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html lang='ja'>
 <head>
